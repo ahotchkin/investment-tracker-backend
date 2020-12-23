7976c0272596b1974ea378cc1a6093a3b0ce2ca1
ahotchkin <allyson.hotchkin@gmail.com>
2020-12-22 21:13:42 -0500

    Move controllers to api/v1, update routes as needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :user_stocks
-  resources :stocks
-  resources :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :user_stocks
+      resources :stocks
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
