599fa744fbad479ad3a8a87b90542435e2f5e34b
ahotchkin <allyson.hotchkin@gmail.com>
2020-12-24 17:40:31 -0500

    Add commented line for Cookies that will be needed later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+
+  #  letting ApplicationController know you've turned these things on in application.rb
+  # include ::ActionController::Cookies
+
 end
