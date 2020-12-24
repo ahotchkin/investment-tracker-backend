27e9c20e9fcc4a71e176a584b5312d78ac6617b3
ahotchkin <allyson.hotchkin@gmail.com>
2020-12-24 17:41:39 -0500

    Namespace controller name with Api::V1; add index action and render json by calling serializer

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -1,2 +1,8 @@
-class StocksController < ApplicationController
+class Api::V1::StocksController < ApplicationController
+
+  def index
+    stocks = Stock.all
+    render json: StockSerializer.new(stocks), status: 200
+  end
+
 end
--- app/controllers/api/v1/user_stocks_controller.rb
+++ app/controllers/api/v1/user_stocks_controller.rb
@@ -1,2 +1,9 @@
-class UserStocksController < ApplicationController
+class Api::V1::UserStocksController < ApplicationController
+
+  def index
+    user_stocks = UserStock.all
+    
+    render json: UserStockSerializer.new(user_stocks), status: 200
+  end
+
 end
