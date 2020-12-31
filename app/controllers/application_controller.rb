36c548ec48cf651441c9bfb4d5778c0c9e98f898
ahotchkin <allyson.hotchkin@gmail.com>
2020-12-31 17:52:14 -0500

    Add methods for current_user and logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::API
 
-  #  letting ApplicationController know you've turned these things on in application.rb
-  # include ::ActionController::Cookies
+  # letting ApplicationController know you've turned these things on in application.rb
+  include ::ActionController::Cookies
+
+  # does this need to be private?
+  def current_user
+    current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 
 end
