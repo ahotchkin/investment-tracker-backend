a9bb332016d381faa4505992bcce8c87a38c085a
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-04 22:06:58 -0500

    Render new instance of UserSerializer in create method to control information being pulled, write get_current_user method to render current_user on page refresh

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      render json: user
+      render json: UserSerializer.new(user), status: 200
     else
       render json: {
         error: "Invalid Credentials"
@@ -13,21 +13,14 @@ class Api::V1::SessionsController < ApplicationController
     end
   end
 
-  # def create
-  #   @user = User.find_by(:username => params[:session][:username])
-  #
-  #   if @user && @user.authenticate(params[:session][:password])
-  #     session[:user_id] = @user.id
-  #     render json: UserSerializer.new(@user), status: 200
-  #   else
-  #     render json: {
-  #       error: "Username or Password is incorrect."
-  #     }
-  #   end
-  # end
-
-
   def get_current_user
+    if logged_in?
+      render json: UserSerializer.new(current_user), status: 200
+    else
+      render json: {
+        error: "No one logged in"
+      }
+    end
   end
 
   def destroy
