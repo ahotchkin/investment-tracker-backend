e22da0305c292d9a5c1ac2c46ac6df10b6da5d07
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-06 16:08:22 -0500

    Write create method to create user upon front end form submission with params provided by user, create user_params method to permit params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,23 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user).serialized_json, status: 200
+    else
+      resp = {
+        :error => user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :username, :password, :email, :income)
+    end
 
 end
