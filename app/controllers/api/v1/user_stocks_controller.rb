3e39af3a2e798c6755bf5453489ebd18329d6256
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-14 20:04:26 -0500

    Add private method for user_stock_params, staart writing method to create user_stock in the database at /user_stocks/new

--- app/controllers/api/v1/user_stocks_controller.rb
+++ app/controllers/api/v1/user_stocks_controller.rb
@@ -2,8 +2,18 @@ class Api::V1::UserStocksController < ApplicationController
 
   def index
     user_stocks = UserStock.all
-    
+
     render json: UserStockSerializer.new(user_stocks), status: 200
   end
+
+  def new
+    user_stock = UserStock.create(params)
+  end
+
+  private
+
+    def user_stock_params
+      params.require(:user_stock).permit(:purchase_date, :number_of_shares, :total_spent, :user_id, :stock_id)
+    end
 
 end
